ff21a706e804be6623132c881afe9e7cbe128f75
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-15 13:35:12 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,8 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT("
+  "SELECT COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE "
 end
